ab9882361232f6a08e5af7a9402b5391394759b6
leonardonichele <leonardonichelework@outlook.com>
2021-01-22 13:21:12 -0300

    info-cards responsivos

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,9 +8,6 @@ function NotFound() {
       <h3>ERROR 404 - página não encontrada</h3>
       <p>A página que você estava buscando<br></br>pode ter sido removida ou não existe.</p>
       <a href="/">Retornar</a>
-      <div className="bar">
-        <h4>teste</h4>
-      </div>
     </div>
   );
 }
