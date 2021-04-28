91d5b58244d1aa5ead280973bf3b8aeca4fca50e
leonardonichele <leonardonichelework@outlook.com>
2021-04-28 01:53:43 -0300

    site 2.0 sem media query

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,92 +1,91 @@
-import React from 'react';
+import * as React from 'react';
+import heroImg from '../images/hero.png';
+import linkedinImg from '../images/linkedin.png';
+import facebookImg from '../images/facebook.png';
+import workOne from '../images/workone.jpg';
+import workTwo from '../images/worktwo.jpg';
+import flowslabsImg from '../images/flowslabs.png';
+import cloudImg from '../images/cloud.png';
+import protheusImg from '../images/protheus.png';
+import dbImg from '../images/db.png';
+import dataImg from '../images/data.png';
 import '../styles/landing.css';
-import logoDataScience from '../images/science.png';
-import LinkedIn from '../images/linkedin.png';
-import sql from '../images/sql.png';
-import powerbi from '../images/powerbi.png';
-import protheus from '../images/protheus.png';
-import azure from '../images/azure.png';
 
 function Landing() {
     return (
-        <div id="homepage">
-            <div id="start">
-                <img src={logoDataScience} alt="FlowsLabs" />
-                <br></br>
-                <h1>Juan Felipe Silva do Amarante</h1>
+        <div className="content">
+            <header>
+                <div className="topnav">
+                    <p><b>Juan</b> Amarante</p>
+                </div>
+            </header>
+            <main>
+                <div className="hero">
+                    <img src={heroImg} alt="byjuanamarante"/>
+                    <br/>
+                </div>
                 <div className="about">
-                    <h3>Tecnólogo em Ciências de Dados
-                    <br></br>
-                    Analista Protheus
-                    <br></br>
-                    Desenvolvedor SQL
-                    <br></br>
-                    Desenvolvedor Power BI</h3>
-                    <br></br>
+                    <h1>um pouco sobre<br></br>quem é o Juan Felipe Silva do Amarante.</h1>
+                    <br/>
+                    <p>Estudante de Ciências da Computação, coordenador de equipe, desenvolvedor PowerBI/MSSQL e analista de Protheus.</p>
+                    <p>- Conhecimento do setor a mais de 8 anos.</p>
+                    <br/>
+                    <a href="https://teste.com"><img src={linkedinImg} alt="linkedin"/></a>
+                    <a href="https://teste.com"><img src={facebookImg} alt="linkedin"/></a>
                 </div>
-                <a href="mailto:juanamarant@outlook.com">Contate-me</a>
-                <br></br>
-                <p>Saiba mais sobre mim</p>
-                <p>&darr;</p>
-            </div>
-            <div id="mid">
-                <h1>Prazer em conhecer, aqui está um pouco sobre mim...</h1>
-                <br></br>
-                <div id="box">
-                    <section id="info">
-                        <div className="card">
-                            <h1>Juan Amarante</h1>
-                            <br></br>
-                            <p>Especialista em pesquisa analítica na área de materiais e financeiro.<br></br>Atuando a 8 anos no sitema PROTHEUS e 3 anos no Power BI. </p>
-                        </div>
-                        <div className="card">
-                            <h1>Protheus</h1>
-                            <br></br>
-                            <p>Módulos; PCP, ESTOQUE CUSTOS, GESTÃO DE CONTRATOS, FATURAMENTO e COMPRAS.</p>
-                        </div>
-                        <div className="card">
-                            <h1>SQL</h1>
-                            <br></br>
-                            <p>Desenvolvimento de Views e Procedures.</p>
-                        </div>
-                        <div className="card">
-                            <h1>Power BI</h1>
-                            <br></br>
-                            <p>Desenvolvimento de BI's financeiros, materiais e custos.</p>
-                        </div>
-                    </section>
+                <hr/>
+                <div className="work">
+                    <h1>Algumas realizações</h1>
+                    <img src={workOne} alt="workone"/>
+                    <img src={workTwo} alt="worktwo"/>
                 </div>
-            </div>
-            <div className="tech">
-                <div className="container">
-                    <section className="techlogo-slider">
-                        <div className="slide"><img src={sql} alt="sql"></img></div>
-                        <div className="slide"><img src={azure} alt="azure"></img></div>
-                        <div className="slide"><img src={powerbi} alt=""></img></div>
-                        <div className="slide"><img src={protheus} alt=""></img></div>
-                    </section>
-                </div>
-            </div>
-            <div id="end">
-                <section id="content">
-                    <div className="about-card">
-                        <div className="btn-contact">
-                            <h1>Conexão</h1>
-                            <br></br>
-                            <a href="mailto:juanamarant@outlook.com">Contate-me</a>
+                <div className="knowledge">
+                    <h1>Conhecimentos</h1>
+                    <div className="row">
+                        <div className="column">
+                            <div className="card">
+                                <img src={dbImg} alt="database"/>
+                                <h3>Banco de dados</h3>
+                                <p>Atuação como desenvolvedor de banco de dados<br></br><br></br><br/>Microsoft SQL</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={cloudImg} alt="cloud"/>
+                                <h3>Computação em nuvem</h3>
+                                <p>Utilização de estrutura em nuvem da AZURE.<br></br>(Datafactory + Blob + Storage)</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={protheusImg} alt="totvs"/>
+                                <h3>TOTVS Protheus</h3>
+                                <p>Módulos:<br></br>Pcp, estoque, custos, gestão de contratos, faturamento e compras.</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={dataImg} alt="data"/>
+                                <h3>Análise de dados</h3>
+                                <p>Via PowerBI, desenvolvendo sobre visualizações em finanças, materiais e custos.</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="about-card">
-                        <h1>Redes Sociais</h1>
-                        <br></br>
-                        <a href="https://www.linkedin.com/in/leonardonichele/"><img src={LinkedIn} alt="linkedin"></img></a>
-                    </div>
-                </section>
-                <br></br>
-            </div>
-            <div id="markdown">
-                <h1>Desenvolvido por Leonardo Nichele - FlowsLabs © 2021 - v1.0</h1>
-            </div>
+                </div>
+                <hr/>
+                <div className="contact">
+                    <h1>Contate-me</h1>
+                    <p>Caso tenha interesse em estabelecer contato, clique abaixo.</p>
+                    <br/>
+                    <br/>
+                    <a href="mailto:juanamarant@outlook.com">Enviar mensagem</a>
+                </div>
+            </main>
+            <footer>
+                <div className="flowslabs">
+                    <img src={flowslabsImg} alt="flowslabs"/>
+                </div>
+            </footer>
         </div>
     );
 }
